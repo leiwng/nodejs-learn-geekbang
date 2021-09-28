1f2f01addc546ce2426bed7c1785b5a6474f4afe
Lei Wang <leiwang@orientsoft.cn>
2021-09-28 18:06:01 +0800

    merge geek-nodejs repo

---
+++ chapter2/async/c. promise/1.js
@@ -0,0 +1,22 @@
+/**
+ * promise的状态转换以及通过then获取内容
+ */
+const promise = new Promise(
+    (resolve, reject) => {
+        setTimeout(function () {
+            resolve(3);
+            // reject(new Error(4))
+        }, 500)
+    })
+
+promise
+    .then(function (result) {
+        console.log(result)
+    })
+    // .catch(function (err) {
+    //     console.log(err)
+    // })
+
+setTimeout(() => {
+    console.log(promise)
+}, 800)
